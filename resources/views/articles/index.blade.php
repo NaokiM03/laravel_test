1e15d2aec88b3eda4c401825bc1de1efffb0f8c7
NaokiM03 <mattsun.phone@gmail.com>
2018-11-11 15:48:15 +0900

    一覧ページに新規作成ページへのリンクを追加

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,6 +3,9 @@
 @section('title', '記事一覧')
 
 @section('content')
+  <div>
+    <a href="/articles/create">新規作成</a>
+  </div>
   @foreach ($articles as $article)
     <h4>{{$article->title}}</h4>
     <p>{{$article->body}}</p>
